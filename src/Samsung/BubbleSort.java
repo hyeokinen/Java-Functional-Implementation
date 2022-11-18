d894c21f81844a13f045af5806e7ad8b66c73399
kkunh97@naver.com <kkunh97@naver.com>
2022-11-18 17:46:39 +0900

    feat: error fix

--- src/Samsung/BubbleSort.java
+++ src/Samsung/BubbleSort.java
@@ -1,50 +1,54 @@
 package Samsung;
 
 import java.util.Scanner;
-
 public class BubbleSort {
 
-    // 테스트 코드 입력받아서 몇번 움직였는지를 카운트 해야한다.
-    // 즉 버블정렬을 구현하고 구현한 함수가 몇번 움직이는지를 확인해야한다.
+    public static int Sort(int[] array){
 
-    public static void Sort(int[] array) {
+        int count = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
+        for (int i = 0 ; i < array.length; i++){
+            boolean swapped = false;
+            for (int j = 0; j < array.length -i -1; j++){
                 if (array[j] > array[j + 1]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
-
+                    swapped = true;
                 }
             }
+            if (! swapped )
+                break;
+            count++;
         }
+
+        return count;
+
     }
 
 
     public static void main(String[] args) {
-
+        int count1=0;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the arraySize");
         int nx = scan.nextInt();
         int[] array1 = new int[nx];
-//        int count1=0;
+
         for (int i = 0; i < nx; i++) {
             System.out.println("Enter the number");
             array1[i] = scan.nextInt();
-
         }
 
-        BubbleSort.Sort(array1);
+
+        count1 = BubbleSort.Sort(array1);
+        System.out.printf("회전 횟수는: %d\n", count1);
+        System.out.printf(
+                "(정렬이 완료됐는지 알려면 한 번 더 돌아야 하므로)\n" +
+                        "전체 회전 횟수는: %d \n", count1 + 1);
 
         for (int i = 0; i < array1.length; i++){
             System.out.print(array1[i] + " ");
-
         }
-
-//        for (int i : array1) {
-//            System.out.println(i + " ");
-//        }
 
     }
 
