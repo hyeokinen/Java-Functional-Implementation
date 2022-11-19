8eda61b2960ce3c6f12e6c8f5dc4a527372a4690
kkunh97@naver.com <kkunh97@naver.com>
2022-11-20 01:41:55 +0900

    feat: 숫자 정렬 구현

---
+++ src/Samsung/SW_1966.java
@@ -0,0 +1,31 @@
+package Samsung;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SW_1966 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int N = sc.nextInt();
+        int[] array = new int[N];
+
+        for (int test_case = 0; test_case < T; test_case++){
+            for (int i = 0; i < N; i++){
+                array[i] = sc.nextInt();
+            }
+            Arrays.sort(array);
+
+            System.out.print("#"+(test_case+1)+" ");
+            for (int i = 0; i < N; i++){
+                System.out.print(array[i]+" ");
+            }
+        }
+
+
+
+
+    }
+}
