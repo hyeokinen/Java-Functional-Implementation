1435b93aa660be1bbd97b90e4c255864e78e07a4
kkunh97@naver.com <kkunh97@naver.com>
2022-11-26 14:26:24 +0900

    feat: programmers solution
    두수의 합 구하기문제

---
+++ src/Programmers/Solution1_plus.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+import java.util.Scanner;
+
+public class Solution1_plus {
+    public int solution(int num1, int num2){
+        int answer = (num1 + num2);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        Solution1_plus solution = new Solution1_plus();
+
+        System.out.println(solution.solution(num1, num2));
+    }
+}
