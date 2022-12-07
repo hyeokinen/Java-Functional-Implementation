a5a176f742b291dbcca98a624dce9e7e3c17e1fa
kkunh97@naver.com <kkunh97@naver.com>
2022-12-07 21:41:48 +0900

    feat: Solution11 solve

--- src/Programmers/Solution11.java
+++ src/Programmers/Solution11.java
@@ -7,8 +7,8 @@ public class Solution11 {
     // 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
 
     public static void main(String[] args) {
-        int P = 7;
-        int N;
+        int P = 7; // 피자조각
+        int N; // 사람의 수
         int result = 1;
 
         Scanner scan = new Scanner(System.in);
@@ -16,7 +16,8 @@ public class Solution11 {
         N = scan.nextInt();
 
         if (N > P){
-             = N/P;
+           result  += N/P;
+            System.out.println(result);
         }
 
         if (N <= P){
