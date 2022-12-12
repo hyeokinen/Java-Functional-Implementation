cf398ca450a8ee38b61d097112b2ed19da2181fc
kkunh97@naver.com <kkunh97@naver.com>
2022-12-12 20:21:38 +0900

    Solution15 solve

---
+++ src/Programmers/Solution15.java
@@ -0,0 +1,10 @@
+package Programmers;
+// 나이출력
+// 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+class Solution15 {
+    public int solution(int age) {
+
+        int num = 2022;
+        return 2022-age+1;
+    }
+}
