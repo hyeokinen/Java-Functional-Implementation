7adcb767011bcfe0f396f205362141f452299fb7
kkunh97@naver.com <kkunh97@naver.com>
2022-12-22 17:36:06 +0900

    edit

---
+++ src/Programmers/Solution28.java
@@ -0,0 +1,13 @@
+package Programmers;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        int[] emergency = new int[]{3,76,24};
+        int[] array = new int[emergency.length];
+        for (int i = 0; i < 100; i++){
+            if (emergency[i] > emergency[i+1]){
+                
+            }
+        }
+    }
+}
