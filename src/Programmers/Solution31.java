fb7dd694bd3285a01bbc52f8eae31eea03601f2a
kkunh97@naver.com <kkunh97@naver.com>
2023-01-03 14:28:33 +0900

    Solution 30~32

---
+++ src/Programmers/Solution31.java
@@ -0,0 +1,15 @@
+package Programmers;
+// 점의 위치 구하기
+
+class Solution31 {
+    public int solution(int[] dot) {
+        int result = 0;
+        for(int i = 0; i < dot.length; i++){
+            if(dot[i] > 0 && dot[i+1] > 0) return 1;
+            if(dot[i] > 0 && dot[i+1] < 0) return 4;
+            if(dot[i] < 0 && dot[i+1] > 0) return 2;
+            if(dot[i] < 0 && dot[i+1] < 0) return 3;
+        }
+        return result;
+    }
+}
