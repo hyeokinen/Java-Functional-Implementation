716bbe6f6b6324b24afe7f3ad490ca54e51d3c66
kkunh97@naver.com <kkunh97@naver.com>
2023-01-04 21:41:58 +0900

    edit

--- src/Programmers/Solution32.java
+++ src/Programmers/Solution32.java
@@ -4,6 +4,7 @@ package Programmers;
 //문제 설명
 //문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
 
+
 class Solution32 {
     public int[] solution(String[] strlist) {
         int count = 0;
