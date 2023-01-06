00cf250afcdc2b6e14f0754def9820da11e0c644
kkunh97@naver.com <kkunh97@naver.com>
2023-01-07 00:02:35 +0900

    Solution40 solve

---
+++ src/Programmers/Level0/Solution40.java
@@ -0,0 +1,17 @@
+package Programmers.Level0;
+
+import java.util.Scanner;
+
+public class Solution40 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 1; i <= n; i++){
+            if(i*i == n){
+                System.out.println("Have");
+
+            }
+
+        }
+    }
+}
