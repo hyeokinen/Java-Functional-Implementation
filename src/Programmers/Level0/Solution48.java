71d8ecce9b91b99d7d00155e49d1ec5440ade64e
kkunh97@naver.com <kkunh97@naver.com>
2023-02-20 09:39:15 +0900

    # Solution 48 solve

---
+++ src/Programmers/Level0/Solution48.java
@@ -0,0 +1,8 @@
+package Programmers.Level0;
+
+public class Solution48 {
+    public static void main(String[] args) {
+        int answer - number[2 * (k-1) % numbers.length];
+        return answer;
+    }
+}
