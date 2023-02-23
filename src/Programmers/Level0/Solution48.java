d70236cf03f13e91cde95c36f8e486e9d73434c8
kkunh97@naver.com <kkunh97@naver.com>
2023-02-23 15:39:53 +0900

    # Solution48 solve
    # 가위바위보 문제 재풀이
    # split 과 배열을 생성하여 구현

--- src/Programmers/Level0/Solution48.java
+++ src/Programmers/Level0/Solution48.java
@@ -1,8 +1,26 @@
-//package Programmers.Level0;
-//
-//public class Solution48 {
-//    public static void main(String[] args) {
-//        int answer - number[2 * (k-1) % numbers.length];
-//        return answer;
-//    }
-//}
+package Programmers.Level0;
+// 가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+
+import java.util.Scanner;
+
+public class Solution48 {
+    public static void main(String[] args) {
+        String rsp = "205";
+        String[] div = rsp.split("");
+        String[] save = new String[rsp.length()];
+        for (int i = 0; i < div.length; i++){
+            if (div[i].equals("0")){
+                save[i] = "5";
+            } else if (div[i].equals("2")) {
+                save[i] = "0";
+
+            } else if (div[i].equals("5")) {
+                save[i] = "2";
+            }
+        }
+        for (String answer : save){
+            System.out.println(answer);
+        }
+
+    }
+}
