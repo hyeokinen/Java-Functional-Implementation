48abb8340f048bf990486798b3ac62fbc3732b01
hyeokinen <kkunh97@naver.com>
2023-03-01 15:10:00 +0000

    # decimalHap 문제 보완 # while 문을 통해서 반복 조건 단순화

--- src/Engineer_Information_Processing/decimalHap.java
+++ src/Engineer_Information_Processing/decimalHap.java
@@ -6,33 +6,26 @@ import java.util.Scanner;
 // 숫자를 입력하고 그 숫자 까지의 소수를 전부 더하는 문제이다.
 public class decimalHap {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+       int hap, k, j;
         System.out.println("숫자를 입력하세요.");
-        // 입력할 숫자
-        int num = scan.nextInt();
-//        int sqrt = (int) Math.sqrt(num);
-        // 소수를 저장할 값.
-        int hap = 2;
-        // 소수 판별 변수
-        int x = 0;
+       Scanner scan = new Scanner(System.in);
+       hap = 0;
+       int a = scan.nextInt();
+       k = 2;
 
-        for (int i = 3; i <= num; i++){
-            if (num == 2) {
-                System.out.println("2");
-                break;
-            }
-            for (int j = 2;  j < i; j++){
-                if (i % j == 0){
-                    x++;
-                    if (x == 0){
-                        hap += i;
-                    }
-                    break;
-                }
-            }
-        }
-        System.out.println(hap);
-
-
+       while (true)
+       {
+           j = 2;
+           while (k % j != 0)
+               j++;
+           if (k == j)
+               hap += k;
+           if (k < a)
+               k++;
+           else {
+               System.out.println(hap);
+               break;
+           }
+       }
     }
 }
