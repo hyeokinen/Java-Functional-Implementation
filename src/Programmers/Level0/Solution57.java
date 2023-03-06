ff6ccc4dfd3a2f2c0f51026f64e8caa04b6048fd
kkunh97@naver.com <kkunh97@naver.com>
2023-03-06 18:30:28 +0900

    # Solution 57 solve

--- src/Programmers/Level0/Solution57.java
+++ src/Programmers/Level0/Solution57.java
@@ -2,8 +2,18 @@ package Programmers.Level0;
 
 // 배열 회전시키기
 
-public class Solution57 {
-    public static void main(String[] args) {
-        
+class Solution57 {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if (direction.equals("right")) {
+            answer[0]=numbers[numbers.length-1];
+            System.arraycopy(numbers, 0, answer, 1, numbers.length-1);
+
+        } else {
+            answer[numbers.length-1]=numbers[0];
+            System.arraycopy(numbers, 1, answer, 0, numbers.length-1);
+
+        }
+        return answer;
     }
 }
