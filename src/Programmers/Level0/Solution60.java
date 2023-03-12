a022b438ac232cb683a8fd6f43627b4f795e787e
kkunh97@naver.com <kkunh97@naver.com>
2023-03-12 19:14:05 +0900

    # Solution60 문제 풀이

--- src/Programmers/Level0/Solution60.java
+++ src/Programmers/Level0/Solution60.java
@@ -1,8 +1,20 @@
 package Programmers.Level0;
 // 합성수 찾기
 // 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
-public class Solution60 {
-    public static void main(String[] args) {
+class Solution {
+    public int Solution60(int n) {
+        int answerCount = 0;
+        for (int i = 4; i <= n; i++) {
+            if (isValue(i)) answerCount++;
+        }
+        return answerCount;
+    }
 
+    public static boolean isValue(int number) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(number) ; i++) {
+            if (number % i == 0) count++;
+        }
+        return count >= 2;
     }
 }
