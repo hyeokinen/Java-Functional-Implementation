3497ea8470ab34c732a133873305f7e7f66c28df
hyeokinen <kkunh97@naver.com>
2023-03-17 13:54:15 +0900

    # Solution파일 정리 및 Solution62 문제 파악

---
+++ src/Programmers/Level0/Solution62.java
@@ -0,0 +1,9 @@
+package Programmers.Level0;
+
+public class Solution62 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("%f",pi);
+    }
+}
