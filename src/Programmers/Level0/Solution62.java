f5a7ed13b90df6782c376e9918913991c0c58444
hyeokinen <kkunh97@naver.com>
2023-03-21 13:08:04 +0900

    # Solution62 반족문을 통해 int를 string으로 변환

--- src/Programmers/Level0/Solution62.java
+++ src/Programmers/Level0/Solution62.java
@@ -1,5 +1,6 @@
 package Programmers.Level0;
 
+import java.io.FilterOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,30 +8,57 @@ import java.util.Objects;
 
 // 7의 개수
 // 머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+
+
+/**
+ * 1. 정수형을 문자열로 변환
+ * 2. 문자형 배열을 쪼갠다음에 다시 배열에 저장을 해야한다.
+ * 그러면 배열의 크기를 어떻게 잡아야 할까? 동적할당?
+ *
+ */
+
+
 public class Solution62 {
+//    public static void main(String[] args) {
+//
+//        int[] array = new int[]{7,77,17};
+//        List<String> list = new ArrayList<>();
+//        int count = 0;
+//        String[] seven = new String[array.length];
+//
+//        int a = 0;
+//        for (int i = 0; i < array.length; i++){
+//            seven[i] = Integer.toString(array[i]);
+//
+//            list.add(Arrays.toString(seven[i].split("")));
+//
+//        }
+//    }
+
     public static void main(String[] args) {
 
+        // int형 배열 선언
         int[] array = new int[]{7,77,17};
-        List<String> list = new ArrayList<>();
-        int count = 0;
-        String[] seven = new String[array.length];
 
-        int a = 0;
-        for (int i = 0; i < array.length; i++){
-            seven[i] = Integer.toString(array[i]);
+        // String형 배열 선언
+        String[] str = new String[array.length];
 
-            list.add(Arrays.toString(seven[i].split("")));
+        // 동적배열
+        ArrayList<String> arrayStr = new ArrayList<String>();
 
+        String test = "";
+
+
+        // 반복문을 통해 int -> String 변환
+        for (int i = 0; i < 10; i++){
+            str[i] = Integer.toString(array[i]);
+            arrayStr.add(Arrays.toString(str[i].split("")));
+            test += arrayStr;
+            System.out.print(test);
         }
-        System.out.println(list.get(1));
-//        for (int i = 0; i < list.size(); i++){
-//            for (int j = 0; j < 3; j++){
-//
-//                if (Objects.equals(list.get(i), "7")){
-//                    count++;
-//                }
-//            }
-//        }
+
+
+
 
 
 
