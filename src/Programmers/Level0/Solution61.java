3d2fc155678c3fdaaa41c0be11318dc273625afb
hyeokinen <kkunh97@naver.com>
2023-03-23 17:58:16 +0900

    # Solution61 소인수 분해 List와 소수 구하는 방식으로 풀이중

--- src/Programmers/Level0/Solution61.java
+++ src/Programmers/Level0/Solution61.java
@@ -1,26 +1,42 @@
 package Programmers.Level0;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 
 // 소인수분해
 // 소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
 public class Solution61 {
+
+    // 리스트를 뒤집어서 한번 해보기
     public static void main(String[] args) {
-        int a = 420;
-        List<Integer> list = new ArrayList<>();
-//        int[] array = new int[a];
-
-        for (int i = 2; i <= a; i++){
-            if (a%i == 0){
-
+        int num = 12;
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> str = new ArrayList<>();
+        for (int i = num; i >= 2; i--){
+            if (num % i == 0){
                 list.add(i);
             }
         }
 
-        System.out.println(list);
 
+        Collections.reverse(list);
+
+
+        int a = 0;
+        for (int i = 0; i < list.size(); i++){
+            for (int j = 2; j <= 100; j++){
+                if (list.get(i) % j == 0 ){
+                    list.remove(a);
+                }
+            }
+        }
+
+
+
+
+
+        System.out.println(list);
 
     }
 }
---
+++ src/Programmers/Level0/Solution63.java
@@ -0,0 +1,7 @@
+package Programmers.Level0;
+
+public class Solution63 {
+    public static void main(String[] args) {
+
+    }
+}
