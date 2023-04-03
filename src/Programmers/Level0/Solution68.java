4f7300cc86d8e95d4f9264e2340a7e9f0f462251
hyeokinen <kkunh97@naver.com>
2023-04-03 11:16:42 +0900

    # Solution68 문제풀이 # replace 메소드를 사용하여 문자 숫자로 대체 후 타입 변경

--- src/Programmers/Level0/Solution68.java
+++ src/Programmers/Level0/Solution68.java
@@ -9,5 +9,17 @@ import com.sun.org.apache.xpath.internal.objects.XString;
 public class Solution68 {
     public static void main(String[] args) {
         String numbers = "onetwothreefourfivesixseveneightnine";
+        // replace를 사용하여 해당 값을 정수로 대치
+        numbers = numbers.replace("one","1");
+        numbers = numbers.replace("two","2");
+        numbers = numbers.replace("three","3");
+        numbers = numbers.replace("four","4");
+        numbers = numbers.replace("five","5");
+        numbers = numbers.replace("six","6");
+        numbers = numbers.replace("seven","7");
+        numbers = numbers.replace("eight","8");
+        numbers = numbers.replace("nine","9");
+        int num = Integer.parseInt(numbers);
+        System.out.println(num);
     }
 }
