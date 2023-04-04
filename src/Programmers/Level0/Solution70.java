29b534de89391461a38ca08bef7a9d4c7952d756
hyeokinen <kkunh97@naver.com>
2023-04-04 17:01:22 +0900

    # Solution70 문제풀이 진행 # HashSet, List사용, 공통 문자 삭제 미해결

---
+++ src/Programmers/Level0/Solution70.java
@@ -0,0 +1,22 @@
+package Programmers.Level0;
+
+// 한 번만 등장한 문자
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+// 문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+// 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+public class Solution70 {
+    public static void main(String[] args) {
+        String s = "hello";
+        String[] array = s.split("");
+        HashSet<String> str = new HashSet<>();
+        for (int i = 0; i < array.length; i++){
+            str.add(array[i]);
+        }
+        ArrayList<String> list = new ArrayList<>(str);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
