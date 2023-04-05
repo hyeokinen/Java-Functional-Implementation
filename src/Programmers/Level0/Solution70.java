a0f7c922c83b8dda87de690e969bb4749bdef686
hyeokinen <kkunh97@naver.com>
2023-04-05 17:42:35 +0900

    # Solution70 문제풀이 완료 # HashMap, getOrDefault를 사용하여 문제풀이

--- src/Programmers/Level0/Solution70.java
+++ src/Programmers/Level0/Solution70.java
@@ -10,16 +10,31 @@ import java.util.*;
 public class Solution70 {
     public static void main(String[] args) {
         String s = "hello";
-        List<String > array = Arrays.asList(s.split(""));
-        Collections.sort(array);
-        for (int i = 0; i < array.size()-1; i++ ){
-            if (Objects.equals(array.get(i), array.get(i + 1))){
-                array.remove(i);
-                array.remove(i+1);
-                i = 0;
-            }
+//        List<String > array = Arrays.asList(s.split(""));
+//        Collections.sort(array);
+//        for (int i = 0; i < array.size()-1; i++ ){
+//            if (Objects.equals(array.get(i), array.get(i + 1))){
+//                array.remove(i);
+//                array.remove(i+1);
+//                i = 0;
+//            }
+//        }
+//        System.out.println(array);
+
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 문자열 s를 나눈것을 key 값으로 하여 해시맵에 값을 추가한다.
+        // getOrDefault(key, defaultValue)
+        // key : 값을 가져와야 하는 요소의 키
+        // defaultValue : 지정된 키로 매핑된 값이 없는 경우 반환되어야 하는 기본 값
+        // 반환 값 : 찾는 key가 존재하면 해당 key에 매핑되어 있는 값을 반환하고, 그렇지 않으면 디폴트 값이 반환된다.
+        for (String str : s.split("")){
+            map.put(str, map.getOrDefault(str,0)+1);
         }
-        System.out.println(array);
+        System.out.println(map);
+
+
 
 
         //        HashSet<String> str = new HashSet<>();
