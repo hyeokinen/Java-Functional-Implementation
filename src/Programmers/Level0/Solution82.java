c45509558a027531250733ed0b221ba9245dbcef
hyeokinen <kkunh97@naver.com>
2023-04-13 14:50:56 +0900

    # Solution82 문제풀이 # 규칙성을 찾아서 푸는 간단한 문제

---
+++ src/Programmers/Level0/Solution82.java
@@ -0,0 +1,14 @@
+package Programmers.Level0;
+
+// 종이자르기
+// 머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다.
+// 예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 최소 가위질 세 번이 필요합니다.
+public class Solution82 {
+    public static void main(String[] args) {
+        int M = 2;
+        int N = 5;
+        int answer = 0;
+        answer = (M-1)+((N-1)*M);
+        System.out.println(answer);
+    }
+}
