63f69af5bcca8b1c442f1a931155bc1016144e9a
hyeokinen <kkunh97@naver.com>
2023-04-17 17:29:40 +0900

    # 정보처리기사 알고리즘 # 소수판별 알고리즘 이해

---
+++ src/Engineer_Information_Processing/소수판별.java
@@ -0,0 +1,14 @@
+package Engineer_Information_Processing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 소수판별 {
+    public static void main(String[] args) {
+        int a = 100;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i <= a; i++){
+
+        }
+    }
+}
