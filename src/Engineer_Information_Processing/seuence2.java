fae888e592326799a5d9f14ab67d37e0edc2254c
hyeokinen <kkunh97@naver.com>
2023-04-20 13:24:05 +0900

    # sequence2 문제파악

---
+++ src/Engineer_Information_Processing/seuence2.java
@@ -0,0 +1,10 @@
+package Engineer_Information_Processing;
+
+// 수열2 - 1-2+3-4+~~-100의 합계
+public class seuence2 {
+    public static void main(String[] args) {
+        /*
+        +와 -가 반복적으로 계산되
+         */
+    }
+}
