a044ac43a6ee08a36efaa23f312017735df171e8
hyeokinen <kkunh97@naver.com>
2023-04-21 13:44:39 +0900

    # Solution88 문제풀이 # 조건문을 이용해서 문제풀이

---
+++ src/Programmers/Level0/Solution88.java
@@ -0,0 +1,33 @@
+package Programmers.Level0;
+
+// 캐릭터의 좌표
+
+/*
+머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다.
+예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다.
+머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다.
+캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+
+[0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+ */
+public class Solution88 {
+    public static void main(String[] args) {
+        String[] keyinput = new String[]{"left", "right", "up", "right", "right"};
+        int[] board = new int[]{11,11};
+
+        int x = 0;
+        int y = 0;
+        int[] answer = new int[]{0,0};
+
+        for (int i = 0; i < keyinput.length; i++){
+            if (keyinput[i] == "left") answer[0]--;
+            if (keyinput[i] == "right") answer[0]++;
+            if (keyinput[i] == "up") answer[1]++;
+            if (keyinput[i] == "down") answer[1]--;
+
+
+        }
+        System.out.println(answer[0] +" " +answer[1]);
+
+    }
+}
