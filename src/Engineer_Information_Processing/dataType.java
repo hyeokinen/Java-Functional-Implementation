f329a3c3f2b3716c97d7e8639bdfcd04bb3650f7
hyeokinen <kkunh97@naver.com>
2023-04-24 18:03:51 +0900

    # 비트 연산자 및 정보처리기사 실습

---
+++ src/Engineer_Information_Processing/dataType.java
@@ -0,0 +1,25 @@
+package Engineer_Information_Processing;
+
+import java.util.Random;
+
+public class dataType {
+    public static void main(String[] args) {
+        // 1번 문제
+//        float a = 123456789.0e-5f;
+//        System.out.printf("%f %e\n",a,a);
+
+        // 2번 문제
+//        int a = 10, b = 10, c = 30;
+//        a -= b -=c;
+//        a += c;
+//        String str01 = a <= b ? String.format("%d", a) : String.format("%d", b);
+//        System.out.printf(str01);
+
+        // 3번 문제
+        String str = "Information";
+        System.out.printf("%-10.4s\n", str);
+        System.out.printf("%10.4s\n", str);
+
+
+    }
+}
