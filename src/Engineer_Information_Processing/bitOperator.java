2467d5ed650cc6d5b5f259d7f57c187b5e2f13e6
hyeokinen <kkunh97@naver.com>
2023-04-24 15:05:26 +0900

    # 비트 연산자 개념 공부

---
+++ src/Engineer_Information_Processing/bitOperator.java
@@ -0,0 +1,11 @@
+package Engineer_Information_Processing;
+
+public class bitOperator {
+    public static void main(String[] args) {
+        byte a = 15, b = 19;
+        System.out.printf("%d\n", ~a);
+        System.out.printf("%d\n", a^b);
+        System.out.printf("%d\n", a&b);
+        System.out.printf("%d\n", a|b);
+    }
+}
