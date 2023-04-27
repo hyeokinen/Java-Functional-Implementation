7d0b6baa76f65d90ba9326a34bef216cbd260362
hyeokinen <kkunh97@naver.com>
2023-04-27 17:31:24 +0900

    # decimal 소수판별 보완 # 변수 선언후 조건에 맞을시에만 소수판별

--- src/Engineer_Information_Processing/decimal.java
+++ src/Engineer_Information_Processing/decimal.java
@@ -2,15 +2,18 @@ package Engineer_Information_Processing;
 
 public class decimal {
     public static void main(String[] args) {
-        int num = 8;
+        int num = 13;
         int decimal = 0;
         for (int i = 2; i<num; i++){
-            if (num % i != 0){
-                System.out.println("소수입니다.");
+            if (num % i == 0){
+                decimal++;
             }
-            else {
-                System.out.println("소수가 아닙니다.");
-            }
+
         }
+        if (decimal != 0 ){
+            System.out.println("소수가 아닙니다.");
+        }
+        else System.out.println("소수입니다.");
+
     }
 }
