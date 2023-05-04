fef69d9c224b2619d4b313b5b882e4cd19c20f9d
hyeokinen <kkunh97@naver.com>
2023-05-04 11:10:13 +0900

    # complement 알고리즘 풀이 # 2의 보수 알고리즘 풀이 완료

--- src/Engineer_Information_Processing/complement.java
+++ src/Engineer_Information_Processing/complement.java
@@ -2,7 +2,6 @@ package Engineer_Information_Processing;
 
 // 1의 보수 2의 보수 구하는 알고리즘
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class complement {
@@ -11,6 +10,7 @@ public class complement {
         // 올림을 해줘야 하는 부분을 구현해야 한다.
         Scanner scan = new Scanner(System.in);
         // 보수값 입력
+        System.out.println("값을 입력하세요.");
         String str = scan.next();
         // str배열과 int배열 선언
         String[] strArray = str.split("");
@@ -21,26 +21,40 @@ public class complement {
         int[] twoComplement = new int[intArray.length];
 
         // 반복문을 통해서 자료형 변경
-        for (int i = 0; i < strArray.length; i++){
+        for (int i = 0; i < strArray.length; i++) {
             intArray[i] = Integer.parseInt(strArray[i]);
         }
 
         // 1의 보수 구하기
-        for (int i = 0; i < intArray.length; i++){
-            if (intArray[i] == 0){
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] == 0) {
                 oneComplement[i] = 1;
-            }
-            else oneComplement[i] = 0;
+            } else oneComplement[i] = 0;
+
+            twoComplement[i] = oneComplement[i];
         }
 
         // 2의 보수 구하기
-        for (int i = intArray.length; i > 0; i--){
-            if (oneComplement[i]+1 == 2){
-
+        twoComplement[intArray.length - 1] = twoComplement[intArray.length - 1] + 1;
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            if (twoComplement[i] == 2) {
+                twoComplement[i] = 0;
+                twoComplement[i - 1]++;
             }
         }
 
+
+        // 결과값 출력
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(oneComplement[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(twoComplement[i] + " ");
+        }
 
 
     }
 }
+
