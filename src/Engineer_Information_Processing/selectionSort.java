a893c2e58cc9b05f70738b8ef289a84d4209c5e0
hyeokinen <kkunh97@naver.com>
2023-05-11 09:45:26 +0900

    # selectionSort 선택정렬 알고리즘 풀이

--- src/Engineer_Information_Processing/selectionSort.java
+++ src/Engineer_Information_Processing/selectionSort.java
@@ -5,7 +5,27 @@ package Engineer_Information_Processing;
 // 두 번째 자료를 세 번째 자료부터 마지막 자료까지와 차례대로 비교하여 그 중 가장 작은 값을 찾아 두 번째 위치에 놓는 과정을 반복하며 정렬을 수행한다.
 //1회전을 수행하고 나면 가장 작은 값의 자료가 맨 앞에 오게 되므로 그 다음 회전에서는 두 번째 자료를 가지고 비교한다. 마찬가지로 3회전에서는 세 번째 자료를 정렬한다.
 public class selectionSort {
-    public static void main(String[] args) {
+    public static void SelectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] arr = { 64, 25, 12, 22, 11 };
+        SelectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
